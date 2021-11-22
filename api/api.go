ec0c72bb400559be0091819a6771ea51785c8717
André Roth <neolynx@gmail.com>
2021-11-22 20:46:02 +0000

    backgroud tasks: be less verbose

--- api/api.go
+++ api/api.go
@@ -137,7 +137,6 @@ func maybeRunTaskInBackground(c *gin.Context, name string, resources []string, p
 	// Run this task in background if configured globally or per-request
 	background := truthy(c.DefaultQuery("_async", strconv.FormatBool(context.Config().AsyncAPI)))
 	if background {
-		log.Println("Executing task asynchronously")
 		task, conflictErr := runTaskInBackground(name, resources, proc)
 		if conflictErr != nil {
 			c.AbortWithError(409, conflictErr)
@@ -145,7 +144,6 @@ func maybeRunTaskInBackground(c *gin.Context, name string, resources []string, p
 		}
 		c.JSON(202, task)
 	} else {
-		log.Println("Executing task synchronously")
 		out := context.Progress()
 		detail := task.Detail{}
 		retValue, err := proc(out, &detail)
